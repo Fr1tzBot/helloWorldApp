d6443747e4890622e4d4c881c8a09de2e049c286
Fritz Geib <fritz@standland.org>
2021-02-20 19:01:35 -0500

    add build instructions to readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # helloWorldApp
+
+A simple repository that I'm using to help myself learn basic python packaging
+
+## Building:
+`python setup.py --command-packages=stdeb.command bdist_deb`
--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 setup(
     name = "helloWorldApp",
     version = "1.0",
-    author = "Fritz Geib",
-    author_email = "fritz@standland.org",
     description = "A hello world app",
     license = "MIT",
     url = "https://github.com/Fr1tzBot/helloWorldApp",
@@ -12,7 +10,7 @@ setup(
     entry_points = {
         'console_scripts' : ['helloWorldApp = helloWorldApp.helloWorldApp:main']
     },
-    #data_files = [
-    #    ('share/applications/', ['helloWorldApp.desktop'])
-    #]
+    data_files = [
+        ('share/applications/', ['helloWorldApp.desktop'])
+    ]
 )
